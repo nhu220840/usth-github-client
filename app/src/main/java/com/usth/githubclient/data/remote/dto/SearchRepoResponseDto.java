34f0bef21a1ee5bfd356257901b65d3e8ba37632
nhu220840 <nhu220840@gmail.com>
2025-10-18 09:58:48 +0700

    delete unnecessary code

--- app/src/main/java/com/usth/githubclient/data/remote/dto/SearchRepoResponseDto.java
+++ app/src/main/java/com/usth/githubclient/data/remote/dto/SearchRepoResponseDto.java
@@ -7,27 +7,11 @@ import java.util.List;
  * Lightweight DTO mirroring the GitHub search repositories response payload.
  */
 public final class SearchRepoResponseDto {
-
-    @SerializedName("total_count")
-    private int totalCount;
-
-    @SerializedName("incomplete_results")
-    private boolean incompleteResults;
-
     // This annotation is necessary for Gson to map correctly.
     @SerializedName("items")
     private List<RepoDto> items;
 
     // --- Getters ---
-
-    public int getTotalCount() {
-        return totalCount;
-    }
-
-    public boolean isIncompleteResults() {
-        return incompleteResults;
-    }
-
     public List<RepoDto> getItems() {
         return items;
     }
--- app/src/main/java/com/usth/githubclient/data/remote/dto/SearchUsersResponseDto.java
+++ app/src/main/java/com/usth/githubclient/data/remote/dto/SearchUsersResponseDto.java
@@ -8,22 +8,7 @@ import java.util.List;
  * DTO representing the payload returned by the GitHub search users endpoint.
  */
 public final class SearchUsersResponseDto {
-
-    @SerializedName("total_count")
-    private int totalCount;
-
-    @SerializedName("incomplete_results")
-    private boolean incompleteResults;
-
     private List<UserDto> items;
-
-    public int getTotalCount() {
-        return totalCount;
-    }
-
-    public boolean isIncompleteResults() {
-        return incompleteResults;
-    }
 
     public List<UserDto> getItems() {
         return items;
